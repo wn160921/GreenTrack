5c73734cfc7a40b760017931ec4f2cd674fca902
王宁 <2556405022@qq.com>
2017-11-04 20:17:59 +0800

    2

---
+++ app/src/main/java/com/example/wn/greentrack/side/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.example.wn.greentrack.side;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wn.greentrack.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
---
+++ app/src/main/java/com/example/wn/greentrack/side/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.example.wn.greentrack.side;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wn.greentrack.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
